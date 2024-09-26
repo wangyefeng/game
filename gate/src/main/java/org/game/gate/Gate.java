30a8370ffa725eec14a50878853c172156a7606e
wangyefeng <wangyefeng@tuyoogame.com>
2024-09-26 17:06:58 +0800

    优化关闭JVM业务代码

--- gate/src/main/java/org/game/gate/Gate.java
+++ gate/src/main/java/org/game/gate/Gate.java
@@ -1,17 +1,17 @@
 package org.game.gate;
 
 import io.netty.util.ResourceLeakDetector;
-import jakarta.annotation.PreDestroy;
+import org.game.gate.handler.client.ClientMsgHandler;
 import org.game.gate.handler.logic.LogicMsgHandler;
 import org.game.gate.net.TcpServer;
+import org.game.gate.net.client.LogicClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.game.gate.handler.client.ClientMsgHandler;
-import org.game.gate.net.client.LogicClient;
+import org.springframework.context.ApplicationContext;
 
 import java.util.Collection;
 
@@ -39,19 +39,36 @@ public class Gate implements CommandLineRunner {
 
     private static boolean stopping = false;
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     static {
         // 设置netty的资源泄露检测
         ResourceLeakDetector.setLevel(ResourceLeakDetector.Level.PARANOID);
     }
 
     private void start() throws Exception {
-        registerHandler();
-        logicClient.start();
-        tcpServer.start();
-        log.info("网关服务器启动成功！");
+        synchronized (Gate.class) {
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                synchronized (Gate.class) {
+                    try {
+                        log.info("JVM 正在关闭，请等待...");
+                        close();
+                    } catch (Exception e) {
+                        log.error("关闭服务器异常！", e);
+                    } finally {
+                        SpringApplication.exit(applicationContext);
+                    }
+                    log.info("JVM 已关闭！");
+                }
+            }, "shutdown-hook"));
+            registerHandler();
+            logicClient.start();
+            tcpServer.start();
+            log.info("网关服务器启动成功！");
+        }
     }
 
-    @PreDestroy
     public void close() throws Exception {
         log.info("gate server closing...");
         stopping = true;
@@ -77,7 +94,11 @@ public class Gate implements CommandLineRunner {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(Gate.class, args);
+        synchronized (Gate.class) {
+            SpringApplication application = new SpringApplication(Gate.class);
+            application.setRegisterShutdownHook(false);
+            application.run(args);
+        }
     }
 
     public static boolean isStopping() {
