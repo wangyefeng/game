606c3fe74c4d8d99e1000b288278bbcdba592230
wangyefeng <wangyefeng@tuyoogame.com>
2024-12-02 20:34:43 +0800

    hibernate开启2级缓存示例

--- test/src/main/java/org/game/test/Test.java
+++ test/src/main/java/org/game/test/Test.java
@@ -7,13 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
-/**
- * Hello world!
- */
 @SpringBootApplication
-@EnableCaching  // 启用缓存
 public class Test implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(Test.class);
@@ -28,7 +23,7 @@ public class Test implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         long time = System.currentTimeMillis();
-        cfgActivityDao.findAll();
+        cfgActivityDao.findById("activity_10#20220920");
         log.info("查询耗时：{}ms", (System.currentTimeMillis() - time));
     }
 
