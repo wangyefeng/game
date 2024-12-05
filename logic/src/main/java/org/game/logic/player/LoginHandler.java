aed3a4a6cc77c52ec9a7a5575542c36955872f07
wangyefeng <wangyefeng@tuyoogame.com>
2024-12-05 16:01:04 +0800

    优化

--- logic/src/main/java/org/game/logic/player/LoginHandler.java
+++ logic/src/main/java/org/game/logic/player/LoginHandler.java
@@ -3,18 +3,17 @@ package org.game.logic.player;
 import io.netty.channel.Channel;
 import org.game.logic.data.config.Config;
 import org.game.logic.data.game.entity.PlayerInfo;
+import org.game.logic.data.game.repository.PlayerDao;
 import org.game.logic.handler.ClientMsgHandler;
+import org.game.proto.protocol.ClientToLogicProtocol;
+import org.game.proto.protocol.LogicToClientProtocol;
+import org.game.proto.struct.Common;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.game.logic.data.game.repository.PlayerDao;
-import org.game.proto.protocol.ClientToLogicProtocol;
-import org.game.proto.protocol.LogicToClientProtocol;
-import org.game.proto.struct.Common;
 
 import java.util.ArrayList;
-import java.util.Optional;
 
 @Component
 public class LoginHandler implements ClientMsgHandler<Common.PbInt> {
@@ -30,14 +29,7 @@ public class LoginHandler implements ClientMsgHandler<Common.PbInt> {
         log.info("LoginHandler: playerId: {}, message: {}", playerId, message);
         Player player = Players.getPlayer(playerId);
         if (player == null) {
-            Optional<PlayerInfo> optional = playerDao.findById(playerId);
-            PlayerInfo playerInfo;
-            if (optional.isPresent()) {
-                playerInfo = optional.get();
-            } else {
-                playerInfo = new PlayerInfo(playerId, "test", new ArrayList<>());
-                playerDao.insert(playerInfo);
-            }
+            PlayerInfo playerInfo = playerDao.findById(playerId).orElseGet(() -> playerDao.insert(new PlayerInfo(playerId, "test", new ArrayList<>())));
             player = new Player(playerInfo, channel);
             Players.addPlayer(player);
         } else {
