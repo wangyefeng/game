2e4f98a58c90f02b2ac4b3339ca92f0109730e4c
wangyefeng <wangyefeng@tuyoogame.com>
2024-12-09 15:03:30 +0800

    优化

--- logic/src/main/java/org/game/logic/data/config/ConfigDataSource.java
+++ logic/src/main/java/org/game/logic/data/config/ConfigDataSource.java
@@ -1,9 +1,12 @@
 package org.game.logic.data.config;
 
+import org.springframework.boot.autoconfigure.mongo.MongoConnectionDetails;
 import org.springframework.boot.autoconfigure.mongo.MongoProperties;
+import org.springframework.boot.autoconfigure.mongo.PropertiesMongoConnectionDetails;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoDatabaseFactorySupport;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -19,9 +22,19 @@ public class ConfigDataSource {
     }
 
     @Bean
+    PropertiesMongoConnectionDetails configMongoConnectionDetails(MongoProperties properties) {
+        return new PropertiesMongoConnectionDetails(properties);
+    }
+
+    @Bean
     public MongoTemplate configMongoTemplate() {
-        MongoProperties mongoProperties = configMongoProperties();
-        return new MongoTemplate(new SimpleMongoClientDatabaseFactory(mongoProperties.getUri()));
+        PropertiesMongoConnectionDetails propertiesMongoConnectionDetails = configMongoConnectionDetails(configMongoProperties());
+        return new MongoTemplate(configMongoDatabaseFactory(propertiesMongoConnectionDetails));
+    }
+
+    @Bean
+    MongoDatabaseFactorySupport<?> configMongoDatabaseFactory(MongoConnectionDetails connectionDetails) {
+        return new SimpleMongoClientDatabaseFactory(connectionDetails.getConnectionString().getConnectionString());
     }
 
 }
--- logic/src/main/java/org/game/logic/data/game/GameDataSource.java
+++ logic/src/main/java/org/game/logic/data/game/GameDataSource.java
@@ -1,10 +1,13 @@
 package org.game.logic.data.game;
 
+import org.springframework.boot.autoconfigure.mongo.MongoConnectionDetails;
 import org.springframework.boot.autoconfigure.mongo.MongoProperties;
+import org.springframework.boot.autoconfigure.mongo.PropertiesMongoConnectionDetails;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.mongodb.core.MongoDatabaseFactorySupport;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -22,8 +25,20 @@ public class GameDataSource {
 
     @Bean
     @Primary
+    PropertiesMongoConnectionDetails gameMongoConnectionDetails(MongoProperties properties) {
+        return new PropertiesMongoConnectionDetails(properties);
+    }
+
+    @Bean
+    @Primary
     public MongoTemplate gameMongoTemplate() {
-        MongoProperties mongoProperties = gameMongoProperties();
-        return new MongoTemplate(new SimpleMongoClientDatabaseFactory(mongoProperties.getUri()));
+        PropertiesMongoConnectionDetails propertiesMongoConnectionDetails = gameMongoConnectionDetails(gameMongoProperties());
+        return new MongoTemplate(gameMongoDatabaseFactory(propertiesMongoConnectionDetails));
+    }
+
+    @Bean
+    @Primary
+    MongoDatabaseFactorySupport<?> gameMongoDatabaseFactory(MongoConnectionDetails connectionDetails) {
+        return new SimpleMongoClientDatabaseFactory(connectionDetails.getConnectionString().getConnectionString());
     }
 }
