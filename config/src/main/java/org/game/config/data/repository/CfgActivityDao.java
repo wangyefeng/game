e9f3428a5adb0ddb08020ca2d59b18d1610ef155
wangyefeng <wangyefeng@tuyoogame.com>
2024-12-11 17:16:11 +0800

    把配置表模块独立出来

--- config/src/main/java/org/game/config/data/repository/CfgActivityDao.java
+++ config/src/main/java/org/game/config/data/repository/CfgActivityDao.java
@@ -2,8 +2,6 @@ package org.game.config.data.repository;
 
 import org.game.config.data.entity.CfgActivity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CfgActivityDao extends JpaRepository<CfgActivity, String> {
 }
--- config/src/main/java/org/game/config/data/repository/CfgFisheriesDao.java
+++ config/src/main/java/org/game/config/data/repository/CfgFisheriesDao.java
@@ -2,8 +2,6 @@ package org.game.config.data.repository;
 
 import org.game.config.data.entity.CfgFisheries;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CfgFisheriesDao extends JpaRepository<CfgFisheries, String> {
 }
