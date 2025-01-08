6b449c0f3e53688fa888b7abf625f5f280a5bdbd
wangyefeng <wangyefeng@tuyoogame.com>
2025-01-08 16:22:26 +0800

    优化

--- cache-test/src/main/java/org/game/cache/MyEntity.java
+++ cache-test/src/main/java/org/game/cache/MyEntity.java
@@ -17,7 +17,7 @@ import java.util.Map;
 public class MyEntity extends GameEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
