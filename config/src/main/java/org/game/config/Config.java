fbbd0f8e5034f89f47ab0810ec04294683ea5d81
wangyefeng <wangyefeng@tuyoogame.com>
2025-01-10 11:11:13 +0800

    重载配置表加上全局锁

--- config/src/main/java/org/game/config/Config.java
+++ config/src/main/java/org/game/config/Config.java
@@ -81,15 +81,22 @@ public class Config implements InitializingBean {
     }
 
     public void reloadAllConfig() {
-        long start = System.currentTimeMillis();
-        log.info("开始重新加载配置表...");
+        databaseService.lockDatabase();
         try {
-            Configs.reload(applicationContext.getBeansOfType(CfgService.class).values());
+            long start = System.currentTimeMillis();
+            log.info("开始重新加载配置表...");
+            try {
+                Configs.reload(applicationContext.getBeansOfType(CfgService.class).values());
+            } catch (Exception e) {
+                log.error("重载配置表失败", e);
+                return;
+            }
+            log.info("配置表重新加载完成, 耗时: {}毫秒", System.currentTimeMillis() - start);
         } catch (Exception e) {
-            log.error("重载配置表失败", e);
-            return;
+            throw e;
+        } finally {
+            databaseService.unlockDatabase();
         }
-        log.info("配置表重新加载完成, 耗时: {}毫秒", System.currentTimeMillis() - start);
     }
 
     @Override
