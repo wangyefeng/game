fdf811a31228b9836030964d428a3f792deba102
wangyefeng <wangyefeng@tuyoogame.com>
2025-03-06 09:46:00 +0800

    优化

--- logic/src/main/java/org/game/logic/player/LevelUpMsgHandler.java
+++ logic/src/main/java/org/game/logic/player/LevelUpMsgHandler.java
@@ -6,7 +6,7 @@ import org.game.proto.struct.Common.PbInt;
 import org.springframework.stereotype.Component;
 
 @Component
-public class LevelUpMsgHandlerAb extends PlayerHandler<PbInt> {
+public class LevelUpMsgHandler extends PlayerHandler<PbInt> {
 
     @Override
     protected void handle(Player player, PbInt data, Configs config) {
